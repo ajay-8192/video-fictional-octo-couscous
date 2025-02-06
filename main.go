25ccb591f0b57b428d8c8ba587a185ca24148c0d
ajay-8192 <ajaychowdary3699@gmail.com>
2025-02-06 22:53:58 +0530

    [master]: inital setup

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	
+	fmt.Println("Hello World!!")
+	router := gin.Default()
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to chatter box",
+		})
+	})
+
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Success",
+		})
+	})
+
+	router.Run(":3000")
+}
